f35be9f6f8252861ad3d427a4f1a682051bea370
convexy <shibuya.keigo@gmail.com>
2025-05-06 23:33:27 +0900

    second commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,13 +11,98 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/api/balance/get", async (req, res) => {
+  try {
+    let wallet = await prisma.wallet.findFirst({
+      orderBy: {
+        date: "desc",
+      },
+    });
+    const today = new Date();
+    today.setHours(0, 0, 0, 0)
+    if (wallet != null) {
+      const tr = await prisma.transactionRecord.findFirst({
+        where: {
+          reflected: {
+            equals: false
+          },
+          date: {
+            gt: wallet.date,
+            lte: today,
+          }
+        },
+        orderBy: {
+          date: "asc",
+        },
+      });
+      if (tr != null) {
+        const trs = await prisma.transactionRecord.findMany({
+          where: {
+            date: {
+              gt: wallet.date,
+              lte: today,
+            },
+          },
+        });
+        trs.forEach(tr => wallet!.amount += tr.type === "income" ? tr.amount : -tr.amount);
+        await prisma.transactionRecord.updateMany({
+          where: {
+            date: {
+              gt: wallet.date,
+              lte: today,
+            },
+          },
+          data: {
+            reflected: true,
+          },
+        });
+      }
+      wallet = await prisma.wallet.upsert({
+        where: {
+          date: today,
+        },
+        create: {
+          date: today,
+          amount: wallet.amount,
+        },
+        update: {
+          amount: wallet.amount
+        }
+      });
+    }
+    else {
+      wallet = await prisma.wallet.create({
+        data: { date: today, amount: 0 },
+      });
+    }
+    res.json(wallet);
+  }
+  catch (err) {
+    console.error((err as Error).message);
+    res.status(400).json({ error: "Getting balance faild.", details: (err as Error).message });
+  }
+});
+
 app.post("/api/transaction/register", async (req, res) => {
   const { date, type, amount, note } = req.body;
   try {
-    const transaction = await prisma.transactionRecord.create({
-      data: { date: new Date(date), type, amount, note }
+    const validatedDate = new Date(date);
+    const count = await prisma.wallet.count({
+      where: {
+        date: {
+          lt: validatedDate
+        },
+      },
     });
-    res.json(transaction);
+    if (count > 0) {
+      const transaction = await prisma.transactionRecord.create({
+        data: { date: validatedDate, type, amount, note }
+      });
+      res.status(201).json(transaction);
+    }
+    else {
+      res.status(400).json({ error: "Register faild.", detail: "Wallet doesn't exist before the date." });
+    }
   }
   catch (err) {
     res.status(400).json({ error: "Register faild.", details: (err as Error).message });
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,17 +1,38 @@
 <template>
   <div>
-    <h1> Welcome to CWallet!</h1>
+    <p><strong>所持金</strong>{{ amount }}円</p>
+  </div>
+  <div>
     <button @click="goToRegisterTransaction">Register Transaction...</button>
     <button @click="goToAbout">Go To About...</button>
   </div>
 </template>
 
-
-<script setup>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 const router = useRouter();
 const goToRegisterTransaction = () => {router.push("/transaction/register")};
 const goToAbout = () => {  router.push('/about');};
+
+const amount = ref<number | null>(null);
+onMounted(async () => {
+  try{
+    const res = await fetch("http://localhost:8080/api/balance/get", {
+      method: "GET",
+    });
+    if (res.ok) {
+      const wallet = await res.json();
+      amount.value = wallet.amount;
+    }
+    else{
+      console.error(res.body);
+    }
+  } 
+  catch (err) {
+    console.error((err as Error).message);
+  }
+});
 </script>
 
 <style scoped>
